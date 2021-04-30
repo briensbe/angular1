f6e29f1f2862ba147f8bf40029a6a34d1c99b0a0
briensbe <briensbe@yahoo.com>
2021-04-30 22:50:09 +0200

    Angular from scratch ( A to Z ) - 1:03:25 - avant refactoring dans les services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AppareilService} from './services/appareil.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mon-projet-angular';
+  isAuth = false;
+
+  //lastUpdate = new Date();
+
+  //écriture avec une Promise
+  lastUpdate = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Async Work Complete");
+      const date = new Date();
+      resolve(date);
+    }, 2000);
+  });
+  
+
+  appareils = [
+    {
+      name: 'Télévision',
+      status: 'éteint'
+    },
+    {
+      name: 'Hifi',
+      status: 'allumé'
+    },
+    {
+      name: 'Lave-vaisselle',
+      status: 'éteint'
+    }
+  ];
+
+  // appOne = "Télé";
+  // appTwo = "Hifi";
+  // appThree = "Lave-vaisselle";
+
+  constructor(private appareilService: AppareilService) {
+    setTimeout(() => { this.isAuth = true; }, 4000);
+  }
+onAllumer() {
+  console.log("Tout Allumer");
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { AppareilService } from './services/appareil.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppareilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    AppareilService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
