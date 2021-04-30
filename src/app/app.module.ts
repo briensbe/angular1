fb322650fafe3dbf4c9a627f9d5bd3c1c2e53b65
briensbe <briensbe@yahoo.com>
2021-04-30 23:52:06 +0200

    1h22'20' - ajout des routes sans modif de l'appli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { AppareilComponent } from './appareil/appareil.component';
 import { AppareilService } from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'appareils', component: AppareilViewComponent},
+  {path: 'auth', component: AuthComponent},
+  {path: '', component: AppareilViewComponent}
+]
 
 @NgModule({
   declarations: [
@@ -17,7 +24,8 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule, 
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AppareilService
