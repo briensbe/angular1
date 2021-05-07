63ba26dd2c2c526b621639e1c811f87d65dc292a
briensbe <briensbe@yahoo.com>
2021-05-07 18:11:49 +0200

    1h59 - Observable - un counter simple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { interval } from 'rxjs';
 
 
 @Component({
@@ -7,5 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  secondes: number;
+
+  constructor() { }
+
+  ngOnInit() {
+    const counter = interval(1000);
+    counter.subscribe(
+      (value: number) => {
+        this.secondes = value;
+      },
+      () => {
+        console.log('Un erreur s\'est produite');
+      },
+      () => {
+        console.log('Observable complétée');
+      }
+    );
+  }
 
 }
